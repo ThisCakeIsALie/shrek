7e702b7b517241fad313ddbe1385fd62b5b8d246
ThisCakeIsALie <37264026+ThisCakeIsALie@users.noreply.github.com>
2019-05-10 20:31:43 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import shrekDab from "./assets/shrek-dab.gif";
+import shrekNoDab from "./assets/shrek-no-dab.png";
+import "./App.css";
+import { start } from "repl";
+
+const App: React.FC = () => {
+  const [isDabbing, setDabbing] = useState(false);
+
+  const savedCounter = window.localStorage.getItem('dabcounter');
+  let initialCounter = 0;
+  if (savedCounter !== null) {
+    initialCounter = parseInt(savedCounter);
+  }
+  const [dabCounter, setDabCounter] = useState(initialCounter);
+
+  function dab() {
+    const newDabCounter = dabCounter + 1;
+    setDabCounter(newDabCounter);
+    window.localStorage.setItem('dabcounter', String(newDabCounter));
+  }
+
+  function startDabbing(e: React.SyntheticEvent) {
+    setDabbing(true);
+    dab();
+    e.preventDefault();
+  }
+
+  function stopDabbing(e: React.SyntheticEvent) {
+    setDabbing(false);
+  }
+
+  return (
+    <div
+      className="App"
+      onTouchStart={startDabbing}
+      onMouseDown={startDabbing}
+      onTouchEnd={stopDabbing}
+      onMouseUp={stopDabbing}
+    >
+      <div className="App-image-container">
+        <img
+          className="App-shrek-image"
+          src={isDabbing ? shrekDab : shrekNoDab}
+          alt="logo"
+        />
+      </div>
+      <div className="App-counter-container">
+        <h1>
+          <p>Shrek has dabbed {dabCounter} times</p>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
