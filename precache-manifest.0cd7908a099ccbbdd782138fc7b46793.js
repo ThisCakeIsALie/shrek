80f2082b80ff2a60b19ea9cf28e7e708163cb62a
ThisCakeIsALie <37264026+ThisCakeIsALie@users.noreply.github.com>
2019-05-10 20:39:04 +0200

    Updates

---
+++ precache-manifest.0cd7908a099ccbbdd782138fc7b46793.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd53b202ae49d2597e91336f2ee6e676",
+    "url": "/shrek/index.html"
+  },
+  {
+    "revision": "88f0be5d6d6cd55fb254",
+    "url": "/shrek/static/css/main.37404255.chunk.css"
+  },
+  {
+    "revision": "cb3172cd3b50fb1f7f2b",
+    "url": "/shrek/static/js/2.2b91e2f1.chunk.js"
+  },
+  {
+    "revision": "88f0be5d6d6cd55fb254",
+    "url": "/shrek/static/js/main.7bd4946f.chunk.js"
+  },
+  {
+    "revision": "b3204b3c0a8ffb55b607",
+    "url": "/shrek/static/js/runtime~main.36cba68e.js"
+  },
+  {
+    "revision": "761c33d2e20ec7b6f08ecd1530aac9dd",
+    "url": "/shrek/static/media/shrek-dab.761c33d2.gif"
+  },
+  {
+    "revision": "0be7510d1b3d8032fd17a79ca33e330d",
+    "url": "/shrek/static/media/shrek-no-dab.0be7510d.png"
+  }
+]);
